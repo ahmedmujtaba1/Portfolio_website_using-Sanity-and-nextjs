151abf9b656ea0de8876986c563cd34d337ae24c
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-08-18 17:47:09 +0500

    Making Profile Schema

---
+++ sanity-studio/schemas/profile.ts
@@ -0,0 +1,12 @@
+import { defineField } from "sanity";
+import { BiUser } from "react-icons/bi";
+
+const profile = {
+  name: "profile",
+  title: "Profile",
+  type: "document",
+  icon: BiUser,
+  fields: [],
+};
+
+export default profile;
