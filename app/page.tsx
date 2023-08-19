a552e8b43df72bb6f62be38a58bf59c2edba94b1
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-08-19 14:15:23 +0500

    Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,84 @@
-"use client";
 import Image from "next/image";
+import Link from "next/link";
 import { getProfile } from "@/sanity/lib/sanity.query";
 import type { ProfileType } from "@/types";
-import Link from "next/link";
-import About from "./components/about";
-import Contact from "./components/contact";
-import Experience from "./components/experience";
-import Intro from "./components/intro";
-import Projects from "./components/projects";
-import SectionDivider from "./components/section-divider";
-import Skills from "./components/skills";
+import { PortableText } from "@portabletext/react";
+import { BiEnvelope, BiFile } from "react-icons/bi";
 
-export default async function Home() {
+export default async function About() {
   const profile: ProfileType[] = await getProfile();
 
   return (
-    <>
-      {/* <button onClick={() => console.log("Hey")}>Click Me</button> */}
-      {/* <main className="max-w-7xl mx-auto lg:px-16 px-6">
-      <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
-        {profile &&
-          profile.map((data) => (
-            <div key={data._id} className="lg:max-w-2xl max-w-2xl">
-              <h1 className="text-3xl font-bold tracking-tight sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
-                {data.headline}
-              </h1>
-              <p className="text-base text-zinc-400 leading-relaxed">
-                {data.shortBio}
+    <main className="lg:max-w-7xl mx-auto max-w-3xl md:px-16 px-6">
+      {profile &&
+        profile.map((data) => (
+          <div key={data._id}>
+            <h1>{data.fullName}</h1>
+            <section className="grid lg:grid-cols-2 grid-cols-1 gap-x-6 justify-items-center">
+              <div className="order-2 lg:order-none">
+                <h1 className="lg:text-5xl text-4xl lg:leading-tight basis-1/2 font-bold mb-8">
+                  I&apos;m {data.fullName}. I live in {data.location}, where I
+                  design the future.
+                </h1>
+
+                <div className="flex flex-col gap-y-3 text-zinc-400 leading-relaxed">
+                  <PortableText value={data.fullBio} />
+                </div>
+              </div>
+
+              <div className="flex flex-col lg:justify-self-center justify-self-start gap-y-8 lg:order-1 order-none mb-12">
+                <div>
+                  <Image
+                    className="rounded-2xl mb-4 object-cover max-h-96 min-h-96 bg-top bg-[#1d1d20]"
+                    src={data.profileImage.image}
+                    width={400}
+                    height={400}
+                    quality={100}
+                    alt={data.profileImage.alt}
+                  />
+
+                  <Link
+                    href={`${data.resumeURL}?dl=${data.fullName}_resume`}
+                    className="flex items-center justify-center gap-x-2 bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md duration-200 py-2 text-center cursor-cell font-medium"
+                  >
+                    <BiFile className="text-base" /> Download Resumé
+                  </Link>
+                </div>
+
+                <ul>
+                  <li>
+                    <Link
+                      href={`mailto:${data.email}`}
+                      className="flex items-center gap-x-2 hover:text-purple-400 duration-300"
+                    >
+                      <BiEnvelope className="text-lg" />
+                      {data.email}
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </section>
+
+            <section className="mt-24 max-w-2xl">
+              <h2 className="font-semibold text-4xl mb-4">Expertise</h2>
+              <p className="text-zinc-400 max-w-lg">
+                I&apos;ve spent few years working on my skills. In no particular
+                order, here are a few of them.
               </p>
-              <ul className="flex items-center gap-x-6 my-10">
-                {Object.entries(data.socialLinks)
-                  .sort()
-                  .map(([key, value], id) => (
-                    <li key={id}>
-                      <a
-                        href={value}
-                        rel="noreferer noopener"
-                        className="flex items-center gap-x-3 mb-5 hover:text-purple-400 duration-300"
-                      >
-                        {key[0].toUpperCase() + key.toLowerCase().slice(1)}
-                      </a>
-                    </li>
-                  ))}
+
+              <ul className="flex flex-wrap items-center gap-3 mt-8">
+                {/* {data.skills.map((skill, id) => (
+                  <li
+                    key={id}
+                    className="bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md px-2 py-1"
+                  >
+                    {skill}
+                  </li>
+                ))} */}
               </ul>
-            </div>
-          ))}
-      </section>
-    </main> */}
-    <main className="flex flex-col items-center px-4">
-      <Intro />
-      <SectionDivider />
-      <About />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Contact />
+            </section>
+          </div>
+        ))}
     </main>
-    </>
   );
 }
