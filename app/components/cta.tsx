d03625cc9fdf4a1970398f2f7fd2e90f39dba991
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-08-19 21:02:06 +0500

    Updating Navbar & CTA

--- app/components/cta.tsx
+++ app/components/cta.tsx
@@ -55,7 +55,7 @@ export const Cta = () => {
   return (
     <>
       <div className="md:mx-auto md:container px-4">
-        <div className="pt-10 md:pt-40">
+        <div className="pt-10 md:pt-32">
           <div className="container mx-auto">
             <div className="flex flex-wrap items-center pb-12">
               <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
@@ -96,75 +96,43 @@ export const Cta = () => {
               <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
                 <img
                   className="absolute w-full h-full inset-0 object-cover object-center rounded-md"
-                  src="https://cdn.tuk.dev/assets/templates/prodify/ProductAdoption.png"
-                  alt=""
+                  src="https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+                  alt="Developer"
                 />
                 <div className="relative z-10 bg-white rounded shadow p-6 w-10/12 -mb-20">
                   <div className="flex items-center justify-between w-full sm:w-full mb-8">
                     <div className="flex items-center">
                       <div className="p-4 bg-yellow-200 rounded-md">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-discount"
-                          width={32}
-                          height={32}
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path stroke="none" d="M0 0h24v24H0z" />
-                          <line x1={9} y1={15} x2={15} y2={9} />
-                          <circle cx="9.5" cy="9.5" r=".5" />
-                          <circle cx="14.5" cy="14.5" r=".5" />
-                          <path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7a2.2 2.2 0 0 0 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1a2.2 2.2 0 0 0 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55 v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55 v-1" />
-                        </svg>
+                      <i className="fa-solid fa-code icon icon-tabler icon-tabler-discount"></i>
                       </div>
                       <div className="ml-6">
                         <h3 className="mb-1 leading-5 text-gray-800 font-bold text-2xl">
-                          2,330
+                          50 +
                         </h3>
                         <p className="text-gray-600 text-sm tracking-normal font-normal leading-5">
-                          Avg Sales
+                          Projects
                         </p>
                       </div>
                     </div>
                     <div>
                       <div className="flex items-center pl-3 text-green-400">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-trending-up"
-                          width={24}
-                          height={24}
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path stroke="none" d="M0 0h24v24H0z" />
-                          <polyline points="3 17 9 11 13 15 21 7" />
-                          <polyline points="14 7 21 7 21 14" />
-                        </svg>
+                        
                         <p className="text-green-400 text-xs tracking-wide font-bold leading-normal pl-1">
-                          7.2%
+                          2 yrs
                         </p>
                       </div>
                       <p className="font-normal text-xs text-right leading-4 text-green-400 tracking-normal">
-                        Increase
+                        Experience
                       </p>
                     </div>
                   </div>
-                  <div className="relative mb-3">
+                  {/* <div className="relative mb-3">
                     <hr className="h-1 rounded-sm bg-gray-200" />
                     <hr className="absolute top-0 h-1 w-7/12 rounded-sm bg-indigo-700" />
                   </div>
                   <h4 className="text-base text-gray-600 font-normal tracking-normal leading-5">
                     Yearly Goal
-                  </h4>
+                  </h4> */}
                 </div>
               </div>
             </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -118,97 +118,23 @@ export function Navbar() {
           className={`${show ? "block" : "hidden"} sm:hidden mt-4 mx-auto`}
         >
           <div className="flex flex-row items-center justify-center space-x-6">
-            <svg
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M23 3.00005C22.0424 3.67552 20.9821 4.19216 19.86 4.53005C19.2577 3.83756 18.4573 3.34674 17.567 3.12397C16.6767 2.90121 15.7395 2.95724 14.8821 3.2845C14.0247 3.61176 13.2884 4.19445 12.773 4.95376C12.2575 5.71308 11.9877 6.61238 12 7.53005V8.53005C10.2426 8.57561 8.50127 8.18586 6.93101 7.39549C5.36074 6.60513 4.01032 5.43868 3 4.00005C3 4.00005 -1 13 8 17C5.94053 18.398 3.48716 19.099 1 19C10 24 21 19 21 7.50005C20.9991 7.2215 20.9723 6.94364 20.92 6.67005C21.9406 5.66354 22.6608 4.39276 23 3.00005V3.00005Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <svg
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M18 2H15C13.6739 2 12.4021 2.52678 11.4645 3.46447C10.5268 4.40215 10 5.67392 10 7V10H7V14H10V22H14V14H17L18 10H14V7C14 6.73478 14.1054 6.48043 14.2929 6.29289C14.4804 6.10536 14.7348 6 15 6H18V2Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <svg
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8V8Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M6 9H2V21H6V9Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <svg
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M16.0002 11.3701C16.1236 12.2023 15.9815 13.0523 15.594 13.7991C15.2065 14.5459 14.5933 15.1515 13.8418 15.5297C13.0903 15.908 12.2386 16.0397 11.408 15.906C10.5773 15.7723 9.80996 15.3801 9.21503 14.7852C8.62011 14.1903 8.22793 13.4229 8.09426 12.5923C7.9606 11.7616 8.09226 10.91 8.47052 10.1584C8.84878 9.40691 9.45438 8.7938 10.2012 8.4063C10.948 8.0188 11.7979 7.87665 12.6302 8.00006C13.4791 8.12594 14.265 8.52152 14.8719 9.12836C15.4787 9.73521 15.8743 10.5211 16.0002 11.3701Z"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M17.5 6.5H17.51"
-                stroke="#1F2937"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+          <Link href="https://twitter.com/Ahmed_Mujtaba69" target="_blank">
+                <i className="fa-brands fa-twitter fa-bounce fa-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer hover:text-blue-600"></i>
+            </Link>
+            <Link href="https://linkedin.com/in/creative-programmer" target="_blank">
+                <i className="fa-brands fa-linkedin-in fa-bounce fa-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer hover:text-blue-600"></i>
+            </Link>
+            <Link href="https://github.com/ahmedmujtaba1" target="_blank">
+                <i className="fa-brands fa-github fa-bounce fa-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer hover:text-blue-600"></i>
+            </Link>
+            <Link href="https://www.facebook.com/profile.php?id=100090336582213" target="_blank">
+                <i className="fa-brands fa-facebook fa-bounce fa-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer hover:text-blue-600"></i>
+            </Link>
           </div>
           <div className="flex flex-col gap-4 mt-4 w-80 mx-auto ">
-            <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-indigo-700 bg-indigo-600 bg-opacity-0 hover:opacity-100 border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
-              Sign Up
-            </button>
+          
             <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
-              Sign In
+              Contact Us
             </button>
           </div>
         </div>
