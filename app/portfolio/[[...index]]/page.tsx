68b87b1e5115417bf1ed140ddcac21f52b1d02ff
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-08-19 08:10:03 +0500

    Changing the file directory

--- app/portfolio/[[...index]]/page.tsx
+++ app/portfolio/[[...index]]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity-studio/sanity.config";
+import config from "@/sanity.config";
 
 export default function Studio() {
   return <NextStudio config={config} />;
