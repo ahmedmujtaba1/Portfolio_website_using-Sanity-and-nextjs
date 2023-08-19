ae42243e3daae635a930207bd626e977831d1518
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-08-19 09:03:57 +0500

    Fetching data from database

---
+++ sanity/lib/sanity.query.ts
@@ -0,0 +1,20 @@
+import { groq } from "next-sanity";
+import client from "./client";
+
+export async function getProfile() {
+  return client.fetch(
+    groq`*[_type == "profile"]{
+      _id,
+      fullName,
+      headline,
+      profileImage {alt, "image": asset->url},
+      shortBio,
+      location,
+      fullBio,
+      email,
+      "resumeURL": resumeURL.asset->url,
+      socialLinks,
+      skills
+    }`
+  );
+}
