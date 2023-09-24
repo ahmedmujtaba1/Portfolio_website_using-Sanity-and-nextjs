6d5affc8d79aba199272351e2e20a8fdf4128fd3
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-09-24 22:18:07 +0500

    Defining services schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -133,6 +133,34 @@ const profile = {
  ],
 };
 
+
+const services = {
+  name: "services",
+  title: "Services",
+  type: "document",
+  fields: [
+    defineField({
+      name: "fontAwesomeClass",
+      title: "Font Awesome Class",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+      rows: 4,
+      validation: (rule) => rule.required().min(10).max(500),
+    }),
+  ],
+};
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile],
+  types: [profile, services],
 }
